7bf1d0b7376eb02e523360c812b4f838cd7f3a3b
Huw <huw@bloowareco.uk>
2021-04-02 21:24:43 +0100

    Mandatory pages added

--- strapi-client/src/app/app-routing.module.ts
+++ strapi-client/src/app/app-routing.module.ts
@@ -4,10 +4,16 @@ import { AboutComponent } from './about/about.component';
 import { CollectionComponent } from './collection/collection.component';
 import { ItemComponent } from './collection/item/item.component';
 import { ContactComponent } from './contact/contact.component';
+import { GdprComponent } from './gdpr/gdpr.component';
+import { PrivacyComponent } from './privacy/privacy.component';
+import { TermsComponent } from './terms/terms.component';
 
 const routes: Routes = [
   { path : "contact", component : ContactComponent},
   { path : "about", component : AboutComponent},
+  { path : "privacy", component : PrivacyComponent},
+  { path : "terms", component : TermsComponent},
+  { path : "gdpr", component : GdprComponent},
   { path : ":collection", component : CollectionComponent},
   { path : ":collection/:item", component : ItemComponent}
 ];
